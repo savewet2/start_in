af2e2715a936d3cb235257c71bf8b7d4313a7cc4
savewet2 <105582055+savewet2@users.noreply.github.com>
2024-02-16 09:29:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import cv2
 from car_detection import CarDetector
 from autopilot import Autopilot
-model_path = 'путь_к_модели/ssd_inference_graph.pb'
-label_map_path = 'путь_к_модели/label_map.pbtxt'
 
-# Создание экземпляров классов
-car_detector = CarDetector(model_path, label_map_path)
-autopilot = Autopilot(car_detector)
+model_path = ''
+#label_map_path = ''
+#car_detector = CarDetector(model_path, label_map_path)
 
-# Запуск обработки видеопотока
+autopilot = Autopilot(model_path)
+
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
     ret, frame = cap.read()
@@ -17,9 +16,7 @@ while cap.isOpened():
 
     processed_frame = autopilot.process_frame(frame)
 
-    # Отображение результата
     cv2.imshow('Autopilot', cv2.resize(processed_frame, (800, 600)))
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
